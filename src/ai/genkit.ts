bb408d0b9f8b1fe2a7944ce15e8dda4e63ef762e
Ehsan Faisal <ehsanfaisal189@gmail.com>
2025-11-22 18:56:04 +0000

    this is error an error occured in ther server render.the specific is omi

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {toNextJs} from '@genkit-ai/next';
 
-export const ai = genkit({
+export const ai = toNextJs({
   plugins: [googleAI()],
   model: 'googleai/gemini-2.5-flash',
 });
